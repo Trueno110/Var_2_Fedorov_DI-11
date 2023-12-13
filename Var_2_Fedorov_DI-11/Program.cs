fa09f1c312cc0bfc4a8da3c612f78de05c64fe8d
Trueno110 <Trueno110@yandex.ru>
2023-12-13 13:18:35 +0300

    Добавьте файлы проекта.

---
+++ Var_2_Fedorov_DI-11/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var_2_Fedorov_DI_11
+{
+    internal class Program
+    {
+        
+    }
+}
+
